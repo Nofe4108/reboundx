96aeafba51b3ca1329c233ec8fd7e8f5ae59202a
Noah Ferich <54567748+Nofe4108@users.noreply.github.com>
2020-02-06 12:14:10 -0700

    Test run made for spline

--- examples/spline_test_run/spline_test_run.c
+++ examples/spline_test_run/spline_test_run.c
@@ -6,3 +6,23 @@
 //
 
 #include "spline_test_run.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "rebound.h"
+#include "reboundx.h"
+
+char data_file[1000] = "test_data.txt";
+char *data_pointer;
+data_pointer = &data_file
+
+int main(int argc, char* argv[]){
+struct reb_simulation* sim = reb_create_simulation();
+    
+    struct rebx_extras* rebx = rebx_attach(sim); // initialize reboundx
+    struct rebx_operator* modify_mass = rebx_load_operator(rebx, "spline_test");
+    rebx_add_operator(rebx, spline_test);
+    
+    
+    
+}
